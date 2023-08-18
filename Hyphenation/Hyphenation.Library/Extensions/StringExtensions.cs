5a5488c507ae673d08c2aa90290ac3b99e77dd4d
Влад Осколков <osvlad@outlook.com>
2023-08-18 23:31:20 +0300

    FEAT: Library. Добавление расширения для строк.

---
+++ Hyphenation/Hyphenation.Library/Extensions/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Hyphenation.Library.Extensions
+{
+    using System.Diagnostics;
+
+    internal static class StringExtensions
+    {
+        /// <summary>
+        /// Указывает, действительно ли указанная строка является строкой <see langword="null"/> или пустой строкой ("").
+        /// </summary>
+        /// <param name="value">Строка для проверки.</param>
+        /// <returns>Значение <see langword="true"/>, если строка равена <see langword="null"/> или пустой строке ("");
+        /// в противном случае — значение <see langword="false"/>.</returns>
+        [DebuggerHidden]
+        public static bool IsNullOrEmpty(this string value) => string.IsNullOrEmpty(value);
+    }
+}
